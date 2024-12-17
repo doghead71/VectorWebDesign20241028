a080f8437723b282274d3e9082c8f46dfd71541b
Calmin Scarlett <cscarlett@advantumpcs.com>
2024-12-16 20:22:14 -0500

    use conditional statement

--- website/scripts/basic.js
+++ website/scripts/basic.js
@@ -1,16 +1,25 @@
 function basicOperations() { //function declaration
     var number1 = document.getElementById("num1").value;
     var number2 = document.getElementById("num2").value;
-    if(number1 === ""){
-        number1  = 0;
-    }else{
-        number1 =parseFloat(number1);
-    }
-    if(number2 === ""){
-        number2  = 0;
-    }else{
-        number2 = parseFloat(number2);
-    }
+
+    //use conditional statement instead for brevity
+    number1 = parseFloat(number1 === "" ? 0 : number1);
+    number2 = parseFloat(number2 === "" ? 0 : number2);
+
+    //These lines of code can be replaced with the above code
+    
+    // if(number1 === ""){
+    //     number1  = 0;
+    // }else{
+    //     number1 =parseFloat(number1);
+    // }
+    // if(number2 === ""){
+    //     number2  = 0;
+    // }else{
+    //     number2 = parseFloat(number2);
+    // }
+
+
     var result = number1 + number2; //variable declaration and assignment. variable result stores the sum of 5 and 7. constant and cannot be changed after assignment
     document.getElementById("output-basic").innerText = number1 +"+"+number2+"="+result;
 }
