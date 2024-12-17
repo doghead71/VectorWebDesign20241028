4ea798fc58134a9b47a9bd42520545587fa9ce9a
KacianR <kacian.rich@gmail.com>
2024-12-16 20:17:40 -0500

    added an If statement

--- website/scripts/basic.js
+++ website/scripts/basic.js
@@ -1,6 +1,16 @@
 function basicOperations() { //function declaration
-    var number1 = parseFloat(document.getElementById("num1").value);
-    var number2 = parseFloat(document.getElementById("num2").value);
+    var number1 = document.getElementById("num1").value;
+    var number2 = document.getElementById("num2").value;
+    if(number1 === ""){
+        number1  = 0;
+    }else{
+        number1 =parseFloat(number1);
+    }
+    if(number2 === ""){
+        number2  = 0;
+    }else{
+        number2 = parseFloat(number2);
+    }
     var result = number1 + number2; //variable declaration and assignment. variable result stores the sum of 5 and 7. constant and cannot be changed after assignment
     document.getElementById("output-basic").innerText = number1 +"+"+number2+"="+result;
 }
