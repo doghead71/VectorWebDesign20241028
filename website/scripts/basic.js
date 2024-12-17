cd721abdb5822d88b200c0797f708365dc88d1c0
KacianR <kacian.rich@gmail.com>
2024-12-16 19:57:13 -0500

    updated the basicOperations function

--- website/scripts/basic.js
+++ website/scripts/basic.js
@@ -1,4 +1,6 @@
 function basicOperations() { //function declaration
-    const result = 5 + 7; //variable declaration and assignment. variable result stores the sum of 5 and 7. constant and cannot be changed after assignment
-    document.getElementById("output-basic").innerText = `5 + 7 = ${result}`;
+    var number1 = parseFloat(document.getElementById("num1").value);
+    var number2 = parseFloat(document.getElementById("num2").value);
+    var result = number1 + number2; //variable declaration and assignment. variable result stores the sum of 5 and 7. constant and cannot be changed after assignment
+    document.getElementById("output-basic").innerText = number1 +"+"+number2+"="+result;
 }
