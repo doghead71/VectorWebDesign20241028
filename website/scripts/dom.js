c023395162a1e2b46131dabd20763d66e5795158
KacianR <kacian.rich@gmail.com>
2024-12-16 20:43:06 -0500

    changed the DOM button

--- website/scripts/dom.js
+++ website/scripts/dom.js
@@ -1,3 +1,16 @@
 function changeText() {
-    document.getElementById("text-target").innerText = "The text has been changed!";
+    var button = document.getElementById("button-change");
+    var textTarget = document.getElementById("text-target");
+
+    //validation to change botton and text 
+    if (button.innerText === "Change Text") {
+        textTarget.innerText = "The text has been changed!";
+        button.innerText = "Reset";
+    } else {
+        textTarget.innerText = "This text will change.";
+        button.innerText = "Change Text";
+    }
 }
+
+
+
